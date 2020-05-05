510b2b92f01ffd3ac509b6fa3446a9a05de786d1
Mad Hallé <mad.halle@icloud.com>
2020-05-05 17:54:43 -0600

    User story 2 complete

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
+
 RSpec.describe "shelters index page", type: :feature do
   it "can see shelter name" do
+    shelter_1 = Shelter.create(name: "All the Pets")
+    shelter_2 = Shelter.create(name: "Some of the Pets")
 
+    visit "/shelters"
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
   end
 end
