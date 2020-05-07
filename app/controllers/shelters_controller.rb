3654ac16c422a60a9afcae345fce608012393fa0
Mad Hallé <mad.halle@icloud.com>
2020-05-07 13:46:31 -0600

    User story 4 complete, shelter creation form up

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -8,7 +8,16 @@ class SheltersController < ApplicationController
   end
 
   def new
+  end
 
+  def create
+    Shelter.create(shelter_params)
+    redirect_to "/shelters"
+  end
+
+  private
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
