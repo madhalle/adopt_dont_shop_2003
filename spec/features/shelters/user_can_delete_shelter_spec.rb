6937477b21ef0cd2d0c7adb0db7289e07d69e678
Mad Hallé <mad.halle@icloud.com>
2020-05-08 11:39:01 -0600

    shelter delete feature complete

--- spec/features/shelters/user_can_delete_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe "shelter show page" do
     shelter_1 = Shelter.create!(name: "All the Pets", address: "17 Random Rd", city: "Denver", state: "CO", zip: 80113)
     shelter_2 = Shelter.create!(name: "Some of the Pets", address: "16 Random Rd", city: "Denver", state: "CO", zip: 80113)
 
+    visit "/shelters"
+    expect(page).to have_content(shelter_1.name)
+
     visit "/shelters/#{shelter_1.id}"
 
     click_link "Delete Shelter"
+    
     expect(current_path).to eq("/shelters")
     expect(page).to have_content(shelter_2.name)
     expect(page).to_not have_content(shelter_1.name)
