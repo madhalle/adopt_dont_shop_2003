67a3ecb0046b0fc6e27f4149c79960125b564d0b
Mad Hallé <mad.halle@icloud.com>
2020-05-10 16:49:23 -0600

    pets index link added to shelter edit page

--- spec/features/shelters/user_can_update_shelter_spec.rb
+++ spec/features/shelters/user_can_update_shelter_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe "shelter show page" do
     click_button "Submit"
     expect(current_path).to eq("/shelters/#{shelter_1.id}")
   end
+
+  it "can get to pet index from new page" do
+    shelter_1 = Shelter.create(name: "All the Pets", address: "17 Random Rd", city: "Denver", state: "CO", zip: 80113)
+
+    visit "/shelters/#{shelter_1.id}/edit"
+
+    click_link "Pet Index"
+    expect(current_path).to eq("/pets")
+  end
 end
