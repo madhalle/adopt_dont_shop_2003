5b1a01ae634dff8c792f613bcbdc7c88cda93772
Mad Hallé <mad.halle@icloud.com>
2020-05-10 16:31:39 -0600

    delete pet from index test added to pet index spec

--- spec/features/pets/user_can_see_all_pets_spec.rb
+++ spec/features/pets/user_can_see_all_pets_spec.rb
@@ -63,6 +63,33 @@ RSpec.describe "pet index page" do
     expect(page).to have_content("Gerald the 4th")
 
   end
+  it "can delete pet from index page" do
+    shelter_1 = Shelter.create(name: "All the Pets",
+                                address: "17 Random Rd",
+                                city: "Denver",
+                                state: "CO",
+                                zip: 80113)
+
+    pet1 = shelter_1.pets.create!( image: "https://i.redd.it/ilfdwwjo9zs11.png",
+                        name: "Gerald",
+                        age: 62,
+                        sex: "Male",
+                        shelter_id: shelter_1.id,
+                        description: "Loves long walks on the beach",
+                        adoption_status: "adoptable")
+    pet2 = shelter_1.pets.create!( image: "https://blog.explore.org/wp-content/uploads/2018/09/fat-bear-blog.jpg",
+                        name: "Minnie",
+                        age: 4,
+                        sex: "Female",
+                        shelter_id: shelter_1.id)
+    visit "/pets"
+    click_link "Delete #{pet1.name}"
+    expect(current_path).to eq("/pets")
+
+    expect(page).to_not have_content("Gerald")
+    expect(page).to have_content(pet2.name)
+
+  end
 end
 
 
